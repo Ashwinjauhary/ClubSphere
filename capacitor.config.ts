ed1ae46519d9467bfe2bee97e0767fad6bf06610
Ashwin Jauhary <ashwin2431333@gmail.com>
2026-01-14 16:36:40 +0530

    chore: configure capacitor for live updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.clubsphere.app',
   appName: 'ClubSphere',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'https://club-sphere-sepia.vercel.app',
+    cleartext: true
+  }
 };
 
 export default config;
